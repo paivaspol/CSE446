86afe68ac34ff318dca35037f6f484b5bb1a4311
Stephen Jonany <sjonany@d-69-91-173-247.dhcp4.washington.edu>
2013-05-09 14:37:11 -0700

    Wrote most of the code for linear regressor, but need to finalize data set to proceed

--- JSONtoSQLIngester/src/data/Dataset.java
+++ JSONtoSQLIngester/src/data/Dataset.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.List;
+
 /**
  * 
  * @author pie
@@ -11,14 +13,66 @@ package data;
  * But if we have time and want to do cooler features, I guess we have to expand x
  */
 public class Dataset {
+	private List<Sample> samples;
 	//maybe an iteration method over all samples? so training/testing is easy?
+	//lol, too lazy. might have to replace this?
+	//also, I think we might need to keep a sample id? so it's easy to map back predictions
+	//to a particular sample, idk, might not be necessary
+	public List<Sample> getSamples(){
+		return samples;
+	}
 	
+	//idk, change this to whatever, this is just a stub
 	public static class Sample{
+		//TODO: idk, maybe we need this
+		//and make data set a map , key = sampleid, val = sample
+		private int sampleId;
+		
+		//in (x,y) notation, x = featureVals, Label = label
+		private FeatureValues featureVals;
+		private Label label;
+		
+		public Sample(FeatureValues fVals, Label label){
+			this.featureVals = fVals;
+			this.label = label;
+		}
+		
+		public FeatureValues getFeatureValues(){
+			return featureVals;
+		}
+		
+		public Label getLabel(){
+			return label;
+		}
+		
+	}
+	
+	public static class FeatureValues{
 		private int restaurantId;
 		private int userId;
+		
+		public FeatureValues(int rId, int uId){
+			this.restaurantId = rId;
+			this.userId = uId;
+		}
+		
+		public int getRestaurantId(){
+			return restaurantId;
+		}
+		
+		public int getUserId(){
+			return userId;
+		}
+	}
+	
+	public static class Label{
 		private double rating;
-		//bla23
-		//or maybe should make it explicit and just have two variables : input, and label
-		//so have to make 2 more classes?
+		public Label(double rating){
+			this.rating = rating;
+		}
+		
+		public double getRating(){
+			return rating;
+		}
 	}
 }
