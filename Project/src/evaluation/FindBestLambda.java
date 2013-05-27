4deca9d3fb1cfc7646cd5888f6a014f657ae08c8
Vaspol Ruamviboonsuk <vaspol@cs.washington.edu>
2013-05-27 08:48:47 -0700

    Modified FindBestLambda to find lambda

--- Project/src/evaluation/FindBestLambda.java
+++ Project/src/evaluation/FindBestLambda.java
@@ -5,39 +5,36 @@ import java.io.PrintStream;
 import java.util.Map;
 import java.util.TreeMap;
 
-import model.ExpectedDifferenceDistanceFunction;
-import model.KNNModel;
 import model.Parameters;
-import model.ScaledManhattanDistanceFunction;
+import model.RidgeRegressionModel;
 import data.RealDataset;
 
 public class FindBestLambda {
 
 	private static final String FILENAME = "data.train";
 	private static final int maxLambda = 100000;
-	private static final int lambdaStep = 1;
 	private static final int k = 5;
 	
 	public static void main(String[] args) throws Throwable {
-		int[] KNeighborParams = new int[]{1,5,10,15,20};
 		RealDataset realDataset = new RealDataset(FILENAME);
 		Map<Integer, Double> errorRate = new TreeMap<Integer, Double>();
 		PrintStream out = new PrintStream(new File("lambda.log"));
 		int minLambda = -1;
 		double minErrorRate = Double.MAX_VALUE;
 		long startTime = System.currentTimeMillis();
-		for (int kneighbor : KNeighborParams) {
-			out.println("kneighbor = " + kneighbor);
+		int[] lambdas = { 0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000, 130000, 140000, 150000 };
+		for (int lambda : lambdas) {
+			out.println("lambda = " + lambda);
 			out.print("\t");
 			Parameters param = new Parameters();
-			param.setParam(KNNModel.ParameterKeys.K.name(), String.valueOf(kneighbor));
-			CrossValidation cv = new CrossValidation(new KNNModel(param, new ScaledManhattanDistanceFunction()), realDataset, k);
+			param.setParam(RidgeRegressionModel.ParameterKeys.RIDGE_PENALTY.name(), String.valueOf(lambda));
+			CrossValidation cv = new CrossValidation(new RidgeRegressionModel(param), realDataset, k);
 			double result = cv.crossValidate();
 			out.println("error = " + result);
-			errorRate.put(kneighbor, result);
+			errorRate.put(lambda, result);
 			minErrorRate = Math.min(result, minErrorRate);
 			if (Double.compare(minErrorRate, result) == 0) {
-				minLambda = kneighbor;
+				minLambda = lambda;
 			}
 			out.flush();
 		}
